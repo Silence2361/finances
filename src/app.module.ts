5f837892e3b0eb909f206723c44f2387e8bcfb92
Silence2361 <161140816+Silence2361@users.noreply.github.com>
2024-07-31 17:33:47 +0500

    configured app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,42 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth/auth.service';
-import { AuthController } from './auth/auth.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ObjectionModule } from 'nestjs-objection';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+
+
 @Module({
   imports: [
     ConfigModule.forRoot({
+      isGlobal: true,
       envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    ObjectionModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        console.log('Database host:', configService.get<string>('DATABASE_HOST'));
+        console.log('Database port:', configService.get<number>('DATABASE_PORT'));
+        console.log('Database user:', configService.get<string>('DATABASE_USER'));
+        console.log('Database name:', configService.get<string>('DATABASE_NAME'));
+        return {
+          config: {
+            client: 'pg',
+            connection: {
+              host: configService.get<string>('DATABASE_HOST'),
+              port: configService.get<number>('DATABASE_PORT'),
+              user: configService.get<string>('DATABASE_USER'),
+              password: configService.get<string>('DATABASE_PASSWORD'),
+              database: configService.get<string>('DATABASE_NAME'),
+            },
+          },
+        };
+      },
+      inject: [ConfigService],
     }),
     AuthModule,
     UsersModule,
+
   ],
-  controllers: [AuthController],
-  providers: [AuthService],
+  controllers: [],
 })
 export class AppModule {}
