ff6a1587b68843d60988e90efe2e30ca43d34b38
Silence2361 <161140816+Silence2361@users.noreply.github.com>
2024-08-17 14:24:57 +0500

    feat(auth): renamed interface and added new  interfaces

--- src/auth/interfaces/auth-response.interface.ts
+++ src/auth/interfaces/auth-response.interface.ts
@@ -1,3 +1,19 @@
-export interface AuthResponse {
+export interface IRegisterCredentials {
+  email: string;
+  password: string;
+}
+
+export interface IRegisterResponse {
+  id: number;
+  email: string;
+  role: string;
+}
+
+export interface ILoginCredentials {
+  email: string;
+  password: string;
+}
+
+export interface ILoginResponse {
   access_token: string;
 }
