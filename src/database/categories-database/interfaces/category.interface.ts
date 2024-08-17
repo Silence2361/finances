63da6ed0357176ceb44e25f1ee2ef2bb1d4b2940
Silence2361 <161140816+Silence2361@users.noreply.github.com>
2024-08-17 16:19:11 +0500

    feat(categories): renamed and added new interfaces

--- src/database/categories-database/interfaces/category.interface.ts
+++ src/database/categories-database/interfaces/category.interface.ts
@@ -7,12 +7,28 @@ export interface ICreateCategory {
   name: string;
 }
 
+export interface ICreateCategoryResponse {
+  id: number;
+}
+
 export interface IUpdateCategory {
+  id?: number;
   name?: string;
 }
 
-export interface ICategoryResponse {
+export interface IUpdateCategoryResponse {
   id: number;
+  name: string;
+}
+
+export interface IFindAllCategoriesResponse {
+  id: number;
+  name: string;
+}
+
+export interface IFindCategoryByIdResponse {
+  id: number;
+  name: string;
 }
 
 export interface ICategoryDetails {
