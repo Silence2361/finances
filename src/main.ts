a89e739398033e3f05d7b87de4c640ad60147d10
Silence2361 <161140816+Silence2361@users.noreply.github.com>
2024-08-18 14:05:44 +0500

    feat: move server port configuration to environment variables

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,7 +16,10 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT', 3000);
 
-  await app.listen(3000);
+  await app.listen(port);
+  console.log(`Server is running on http://localhost:${port}`);
 }
 bootstrap();
