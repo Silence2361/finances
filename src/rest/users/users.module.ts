c62895906caf51f0d7ddb69e28dd22e2e3af43d5
Silence2361 <161140816+Silence2361@users.noreply.github.com>
2024-08-20 18:36:57 +0500

    refactor(users): imported global module instead of module of each feature

--- src/rest/users/users.module.ts
+++ src/rest/users/users.module.ts
@@ -4,29 +4,12 @@ import { User } from '../../database/users-database/users.model';
 import { UserController } from './users.controller';
 import { UsersRepository } from '../../database/repositories/user.repository';
 import { DatabaseModule } from '../../database/repositories/repositories.module';
-import { CreateUserModule } from '../../features/users/create-user/create-user.module';
-import { UpdateUserModule } from '../../features/users/update-user/update-user.module';
-import { GetUsersModule } from '../../features/users/get-users/get-users.module';
-import { GetUserByIdModule } from '../../features/users/get-user-by-id/get-user-by-id.module';
-import { DeleteUserByIdModule } from '../../features/users/delete-user-by-id/delete-user-by-id.module';
+import { FeaturesModule } from '../../features/features.module';
 
 @Module({
-  imports: [
-    ObjectionModule.forFeature([User]),
-    DatabaseModule,
-    CreateUserModule,
-    UpdateUserModule,
-    GetUsersModule,
-    GetUserByIdModule,
-    DeleteUserByIdModule,
-  ],
+  imports: [ObjectionModule.forFeature([User]), DatabaseModule, FeaturesModule],
   providers: [UsersRepository],
   controllers: [UserController],
-  exports: [
-    CreateUserModule,
-    UpdateUserModule,
-    ObjectionModule,
-    UsersRepository,
-  ],
+  exports: [ObjectionModule, UsersRepository],
 })
 export class UsersModule {}
