0fb1dcb06f1379fd9ec8bd09c633dfc4edea0ee9
Silence2361 <161140816+Silence2361@users.noreply.github.com>
2024-08-21 12:15:18 +0500

    feat(categories): added validation to DTO

--- src/rest/categories/dto/create-category.dto.ts
+++ src/rest/categories/dto/create-category.dto.ts
@@ -1,8 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class CreateCategoryDto {
   @ApiProperty({ example: 'Food', description: 'The name of the category' })
   @IsString()
+  @IsNotEmpty()
+  @MaxLength(50, { message: 'Name must mot be longer than 50 characters' })
   name: string;
 }
