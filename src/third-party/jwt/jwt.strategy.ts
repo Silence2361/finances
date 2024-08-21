ff67034199aea51729c4a0f20aa6b4be5ef6ecb2
Silence2361 <161140816+Silence2361@users.noreply.github.com>
2024-08-21 13:49:02 +0500

    refactor(auth): removed email from JWT payload and updated validate method in JwtStrategy

--- src/third-party/jwt/jwt.strategy.ts
+++ src/third-party/jwt/jwt.strategy.ts
@@ -2,7 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
-import { JwtPayload } from './payload/jwt.payload';
+import { IValidatedUser, JwtPayload } from './payload/jwt.payload';
 import { UsersRepository } from '../../database/users/user.repository';
 import { IUser } from '../../database/users/user.interface';
 
@@ -19,11 +19,11 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: JwtPayload): Promise<Omit<IUser, 'password'>> {
+  async validate(payload: JwtPayload): Promise<IValidatedUser> {
     const user = await this.usersRepository.findUserById(payload.userId);
     if (!user) {
       throw new UnauthorizedException();
     }
-    return { id: user.id, email: user.email, role: user.role };
+    return { id: user.id, role: user.role };
   }
 }
--- src/third-party/jwt/payload/jwt.payload.ts
+++ src/third-party/jwt/payload/jwt.payload.ts
@@ -1,4 +1,11 @@
+import { UserRole } from '../../../database/users/users.model';
+
 export interface JwtPayload {
   userId: number;
   role: string;
+}
+
+export interface IValidatedUser {
+  id: number;
+  role: UserRole;
 }
