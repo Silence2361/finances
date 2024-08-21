3196cddcfbee7f2919ac4e28341ac80b444091d2
Silence2361 <161140816+Silence2361@users.noreply.github.com>
2024-08-21 15:32:07 +0500

    refactor(users): updated naming of update feature

--- src/features/users/update-user/update-user.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UpdateUserFeature } from './update-user.feature';
-
-@Module({
-  providers: [UpdateUserFeature],
-  exports: [UpdateUserFeature],
-})
-export class UpdateUserModule {}
