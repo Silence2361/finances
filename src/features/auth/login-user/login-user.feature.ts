5dfa1ea40153f90a6c4016e6e74b46046e62414a
Silence2361 <161140816+Silence2361@users.noreply.github.com>
2024-08-21 14:04:33 +0500

    style(auth): added spacing and format cod

--- src/features/auth/login-user/login-user.feature.ts
+++ src/features/auth/login-user/login-user.feature.ts
@@ -24,6 +24,7 @@ export class LoginUserFeature {
     const { email, password } = params;
 
     const user = await this.usersRepository.findUserByEmail(email);
+
     if (!user) {
       throw new NotFoundException('User not found');
     }
@@ -37,6 +38,7 @@ export class LoginUserFeature {
     const payload = { userId: user.id, role: user.role };
 
     const accessToken = await this.jwtService.signAsync(payload);
+
     return { accessToken };
   }
 }
